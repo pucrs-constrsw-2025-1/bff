5c68548d4b24b38ca258b407ef17778ef163cdca
Santiago Firpo <santiagoifirpo4@gmail.com>
2025-06-20 01:29:32 -0300

    Configured cors

--- app.js
+++ app.js
@@ -8,13 +8,23 @@ const port = process.env.BFF_INTERNAL_API_PORT;
 const { checkLoggedIn } = require("./middleware/authMiddleware");
 const {createProxyMiddleware} = require("http-proxy-middleware");
 
+// Configure middleware first
+app.use(cors({
+    origin: `${process.env.FRONTEND_EXTERNAL_PROTOCOL}://${process.env.FRONTEND_EXTERNAL_HOST}:${process.env.FRONTEND_EXTERNAL_PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Configure the proxy middleware
 const createServiceProxy = ({serviceName, serviceUrl}) => (createProxyMiddleware({
-	target: serviceUrl, // The URL of your external service
-	changeOrigin: true, // Recommended for virtual hosted sites, changes the host header to the target URL
-	pathRewrite: {
-		[`^/${serviceName}`]: "", // Rewrite the path: remove "/${serviceName}" from the beginning
-	},
+    target: serviceUrl, // The URL of your external service
+    changeOrigin: true, // Recommended for virtual hosted sites, changes the host header to the target URL
+    pathRewrite: {
+        [`^/${serviceName}`]: "", // Rewrite the path: remove "/${serviceName}" from the beginning
+    },
 }));
 
 // Use the proxy middleware for any requests starting with "/classes"
@@ -26,14 +36,6 @@ app.use("/reservations", checkLoggedIn, createServiceProxy({serviceName: "reserv
 app.use("/resources", checkLoggedIn, createServiceProxy({serviceName: "resources", serviceUrl: `${process.env.RESOURCES_INTERNAL_PROTOCOL}://${process.env.RESOURCES_INTERNAL_HOST}:${process.env.RESOURCES_INTERNAL_API_PORT}`}));
 app.use("/rooms", checkLoggedIn, createServiceProxy({serviceName: "rooms", serviceUrl: `${process.env.ROOMS_INTERNAL_PROTOCOL}://${process.env.ROOMS_INTERNAL_HOST}:${process.env.ROOMS_INTERNAL_API_PORT}`}));
 app.use("/students", checkLoggedIn, createServiceProxy({serviceName: "students", serviceUrl: `${process.env.STUDENTS_INTERNAL_PROTOCOL}://${process.env.STUDENTS_INTERNAL_HOST}:${process.env.STUDENTS_INTERNAL_API_PORT}`}));
-
-app.use(
-	cors({
-		origin: "*",
-	})
-);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.get("/health", (req, res) => {
 	res.status(200).send("OK");
